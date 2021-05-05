f30f5df8f8e53e2fe90a526676fe7486e375d977
Mohamed Hamza <mohamedhamza@aucegypt.edu>
2021-05-05 15:31:02 +0200

    clicking note background closes it

--- src/Note.js
+++ src/Note.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import './Note.css';
 
-const Note = ({ title, body }) => {
+const Note = ({ title, body, setOpenNoteState }) => {
     return (
-        <div className="note-card-wrapper">
+        <div className="note-card-wrapper"
+            onClick={e => {
+                if (e.target.className !== "note-card") {
+                    setOpenNoteState(null);
+                }
+            }}>
             <div className="note-card">
                 <h1>{title}</h1>
                 <p>{body}</p>
